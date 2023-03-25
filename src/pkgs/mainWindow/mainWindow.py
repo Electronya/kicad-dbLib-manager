f7f9bcbfb7a8c1e8135db253b8b52d9a09c838b1
jbacon <julien.baccon.1@gmail.com>
2023-03-24 22:59:45 -0400

    moving createErrorMsgBox to appComposer

--- src/pkgs/mainWindow/mainWindow.py
+++ src/pkgs/mainWindow/mainWindow.py
@@ -1,7 +1,5 @@
 import logging
-import sys
 
-import PySide2.QtCore as qtc
 import PySide2.QtWidgets as qtw
 
 from .mainWindow_auto import Ui_mainWindow
@@ -19,19 +17,3 @@ class MainWindow(qtw.QMainWindow, Ui_mainWindow):
         self._logger = logging.getLogger('app.windows.main')
         self._logger.info('loading UI...')
         self.setupUi(self)
-
-    @qtc.Slot(qtw.QMessageBox.Icon, Exception)
-    def _createErrorMsgBox(self, lvl: qtw.QMessageBox.Icon,
-                           error: Exception) -> None:
-        """
-        Create a error message box.
-        """
-        self._logger.error(f"error: {str(error)}")
-        msgBox = qtw.QMessageBox(self)
-        msgBox.setWindowTitle('Error!!')
-        msgBox.setText(str(error))
-        msgBox.setIcon(lvl)
-        if lvl == qtw.QMessageBox.Critical:
-            self._logger.debug('connecting to button clicked for critical')
-            msgBox.buttonClicked.connect(lambda i: sys.exit(1))
-        msgBox.exec_()
